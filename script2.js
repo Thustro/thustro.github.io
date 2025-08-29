b67d5703dde5fd3db1daf2ed60cc67184c98279e
norfolk <75830830+Thustro@users.noreply.github.com>
2025-08-30 02:02:43 +1000

    Add files via upload

--- script2.js
+++ script2.js
@@ -19,8 +19,8 @@ document.getElementById('randomize').onclick = function() {
 	const noProbe34Vehicles = ['2012 Doghouse', '2014 Doghouse', 'Early 2010 Doghouse', '2010 Filming Doghouse', '2010 Bullhorn Doghouse', '2010 Doghouse', '2006 Elysion Slick Si', '2010 Bullhorn Doghouse', 'Forester/2018 Sumo Woodlands XT', 'Dominator Fore', '2021 Combi Kuma GT-Line', 'Grizzly', '2006 Chevlon Zafiro', '2006 Chevlon Zafiro', '2006 Chevlon Zafiro', '2006 Chevlon Zafiro', '2006 Chevlon Zafiro', '2006 Chevlon Zafiro', '2006 Chevlon Zafiro', '2006 Chevlon Zafiro', 'Dominator 1', '2009 Dominator 1', '2012 Dominator 1', '2011 Dominator 1', '2013 Dominator 1', '2014 Dominator 1', 'TIV 1', 'Dominator 2', '2012 Dominator 2', '2014 Dominator 2', 'Dominator 3', 'TIV 2', '2020 TIV 2', '2009 TIV 2', '2010 TIV 2', '2012 TIV 2', '2015 TIV 2', '2008 TIV 2', '2012 Tornado Alley TIV 2', '2023 TIV 2', '2024 TIV 2'];
 	const noProbe234Vehicles = ['Charger/2017 BullHorn Prancer SXT', '2014 Dominator 2'];
 	const noProbeVehicles = ['DOW 3', 'RaXPol'];
+	const nocustomization = ['Dominator 1', '2009 Dominator 1', '2012 Dominator 1', '2011 Dominator 1', '2013 Dominator 1', '2014 Dominator 1', 'TIV 1', 'Dominator 2', '2012 Dominator 2', '2013 Dominator 2', '2014 Dominator 2', 'Dominator 3', 'TIV 2', '2020 TIV 2', '2009 TIV 2', '2010 TIV 2', '2012 TIV 2', '2015 TIV 2', '2008 TIV 2', '2012 Tornado Alley TIV 2', '2023 TIV 2', '2024 TIV 2', 'DOW 3', 'RaXPol', 'DOW 6'];
 	
-    // === Apply hiding logic ===
     if (noProbeVehicles.includes(text)) {
         document.getElementById("probe1").innerText = '';
         document.getElementById("probe2").innerText = '';
@@ -42,10 +42,15 @@ document.getElementById('randomize').onclick = function() {
         document.getElementById("probe3").innerText = probe3;
         document.getElementById("probe4").innerText = '';
     } else {
-        // Default: show all
         document.getElementById("probe1").innerText = probe1;
         document.getElementById("probe2").innerText = probe2;
         document.getElementById("probe3").innerText = probe3;
         document.getElementById("probe4").innerText = probe4;
+    }
+	
+	    if (nocustomization.includes(text)) {
+        document.getElementById("colour").innerText = '';
+    } else {
+        document.getElementById("colour").innerText = colour;
     }
 };
